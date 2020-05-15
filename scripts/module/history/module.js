09a909a408655d7a781b3a0fb07d3d3c33752ae2
唯一 <fuwy@foxmail.com>
2020-05-15 17:13:13 +0800

    fix: 修正因监听顺序的问题，造成image数据无法正常渲染的问题

--- scripts/module/history/module.js
+++ scripts/module/history/module.js
@@ -550,13 +550,13 @@ App.module.extend('history', function() {
                 'use_time': time,
                 'status': status
             };
-            Model.set('responseData', response_data);
             Model.set('requestFormType', form_data_type);
             Model.set('requestFormTypeTmp', form_data_type);
             Model.set('requestData', historyData[key]);
             Model.set('requestData_' + form_data_type, historyData[key]['data']);
             Model.set('requestHeaders', historyData[key]['request_headers'] ? historyData[key]['request_headers'] : {});
             Model.set('authentication', historyData[key]['authentication']);
+            Model.set('responseData', response_data);
         }
     };
 });
